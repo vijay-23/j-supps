9a59243b3aadd4518f8950b59225d42a2ee77d3d
vijay-23 <54846667+vijay-23@users.noreply.github.com>
2022-06-22 12:20:59 +0200

    blabla

---
+++ about.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+include("includes/main.php");
+
+?>
+
+<?php
+
+include("includes/footer.php");
+
+?>
+<script src="js/jquery.min.js"> </script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
