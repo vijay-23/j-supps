2ea6b55346219e487b1981c8875c386a70520c55
vijay-23 <54846667+vijay-23@users.noreply.github.com>
2022-06-22 12:19:35 +0200

    idk2

---
+++ admin_area/delete_coupon.php
@@ -0,0 +1,20 @@
+<?php
+
+
+if(!isset($_SESSION['admin_email'])){
+
+echo "<script>window.open('login.php','_self')</script>";
+
+}
+
+else {
+
+
+?>
+
+<?php
+
+?>
+
+
+<?php } ?>
---
+++ admin_area/edit_bundle.php
@@ -0,0 +1,16 @@
+<?php
+
+if(!isset($_SESSION['admin_email'])){
+
+echo "<script>window.open('login.php','_self')</script>";
+
+}
+
+else {
+
+?>
+
+<?php
+?>
+
+<?php } ?>
